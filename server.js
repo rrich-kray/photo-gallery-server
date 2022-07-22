175c0902d313a884a8773895a6e04a0a0bb5b54c
Ryan Kray <rrich.kray@gmail.com>
2022-07-21 20:18:04 -0400

    added client to CORS exception list

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ const path = require('path');
 app.use(fileUpload({ createParentPath: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://photo-gallery-client-rrich.herokuapp.com',
+    ],
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
